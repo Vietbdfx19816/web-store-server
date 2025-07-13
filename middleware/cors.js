9683bf60bf060c3065defdb8c8db931edc8d58b4
vietbdfx19816 <vietbdfx19816@funix.edu.vn>
2025-07-13 11:52:15 +0700

    render deploy firebase

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const corsOptions = {
   origin: [
     'https://web-store-client-silk.vercel.app/',
-    'https://apple-store-client-19816.web.app',
+    'https://apple-store-client-19816.web.app/',
   ],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Set-Cookie'],
